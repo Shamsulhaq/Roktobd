8d78217d092f8dc2637487a3ebd7da59d3ede856
Shamsulhaq <bmshamsulhaq65@gmail.com>
2017-09-18 21:20:16 +0600

    Fragment Transaction Commit ok

--- app/src/main/java/com/httproktobd/roktobd/ActivityRegistration.java
+++ app/src/main/java/com/httproktobd/roktobd/ActivityRegistration.java
@@ -1,5 +1,6 @@
 package com.httproktobd.roktobd;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -13,14 +14,12 @@ public class ActivityRegistration extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        /*FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft  =   fm.beginTransaction();
-        FragmenBloodGroup fragmenBloodGroup = new FragmenBloodGroup();
-        ft.replace(R.id.fragment_reg_blood_group,fragmenBloodGroup);
-        ft.commit();*/
-    }
-
-    public void changeFragment(View view) {
+        Fragment bloodgroupFlagment = new FragmenBloodGroup();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.fragmentContainer,bloodgroupFlagment);
+        ft.addToBackStack(null);
+        ft.commit();
 
     }
 }
