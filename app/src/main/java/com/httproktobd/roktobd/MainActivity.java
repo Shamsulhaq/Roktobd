46c9c44d92a28177e5ef978bdc88eb058a3d3986
ashif <ashifcse9@gmail.com>
2017-09-19 22:30:17 +0600

    Registration Form

--- app/src/main/java/com/httproktobd/roktobd/MainActivity.java
+++ app/src/main/java/com/httproktobd/roktobd/MainActivity.java
@@ -1,9 +1,12 @@
 package com.httproktobd.roktobd;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       /* TextView number = (TextView) findViewById(R.id.number);
+        TelephonyManager tManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        number.setText(tManager.getLine1Number());
+    */
+        TextView number = (TextView) findViewById(R.id.number);
+        TelephonyManager tm = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
+        String num = tm.getLine1Number();
+        number.setText(num);
     }
 
     public void linkToSignup(View view) {
